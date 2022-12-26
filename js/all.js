2a1e8527bb9683ea164ff6b811c1345e20328250
Anna Chen <lovesmart734@gmail.com>
2022-12-26 09:12:26 +0800

    update

--- js/all.js
+++ js/all.js
@@ -4,4 +4,10 @@ sidebarBtn.addEventListener('click', (e) => {
     e.preventDefault();
     body.classList.toggle('sidebar-toggled');
 })
-
+const deleteModal = document.querySelector('#deleteModal');
+deleteModal.addEventListener('show.bs.modal', function (e) {
+    let btn = e.relatedTarget;
+    let text = btn.dataset.bsOrderId;
+    const deleteIdText = document.querySelector('#deleteId')
+    deleteIdText.textContent = text;
+})
